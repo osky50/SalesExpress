4e4a3a056f9d4f56ce22a700139b9a377e1adaa3
Oscar Perez <oscar.perez@aperosolutions.com>
2016-10-25 11:47:24 -0600

    ProdDetail view

--- SalesExpress/viewModels/prodDetViewModel.js
+++ SalesExpress/viewModels/prodDetViewModel.js
@@ -12,21 +12,81 @@
         //   show
 
         onBeforeShow: function () {
-            var location = window.location.toString();
-            var predrecno = location.substring(location.lastIndexOf('?') + 4);
-            app.viewModels.prodDetViewModel.jsdoDataSource.filter({ field: "Prod-RecNo", operator: "eq", value: predrecno });
+            var prodDetListView = $("#prodDetailView").data("kendoMobileListView");
+            if (prodDetListView === undefined) { //extra protection in case onInit have not been fired yet
+                app.viewModels.prodDetListViewModel.onInit(this);
+            } else if (prodDetListView.dataSource && prodDetListView.dataSource.data().length === 0) {
+                prodDetListView.dataSource.read();
+            }
+
+            // Set list title to resource name
+            if (app.viewModels.prodDetViewModel.resourceName !== undefined) {
+                app.changeTitle(app.viewModels.prodDetViewModel.resourceName);
+            }
         },
         onInit: function (e) {
             try {
                 // Create Data Source
                 app.viewModels.prodDetViewModel.createJSDODataSource();
+                app.viewModels.prodDetViewModel.createProdLocDataSource();
+
                 app.views.productDetView = e.view;
+                $("#prodDetailView").kendoMobileListView({
+                    dataSource: app.viewModels.prodDetViewModel.jsdoDataSource,
+                    pullToRefresh: false,
+                    style: "display: inline",
+                    appendOnRefresh: false,
+                    endlessScroll: false,
+                    template: kendo.template($("#prodDetailTemplate").html())
+                });
+
+                $("#prodDetailLocView").kendoMobileListView({
+                    dataSource: app.viewModels.prodDetViewModel.prodLocDataSource,
+                    pullToRefresh: false,
+                    style: "display: inline",
+                    appendOnRefresh: false,
+                    endlessScroll: false,
+                    template: kendo.template($("#prodDetLocTemplate").html())
+                });
             }
             catch (ex) {
                 console.log("Error in initproductDetView: " + ex);
             }
         },
+        createProdLocDataSource: function () {
+            var eLoc = kendo.data.Model.define({
+                id: "id", // the identifier is the "id" field (declared below)
+                fields: {
+                    Loc_Id: {
+                        type: "string", // the field is a string
+                        validation: { // validation rules
+                            required: true // the field is required
+                        },
+                        from: "[\"Loc-id\"]",
+                        defaultValue: "<empty>" // default field value
 
+                    }
+                }
+            });
+            this.prodLocDataSource = new kendo.data.DataSource({
+                schema: {
+                    model: eLoc,
+                    parse: function (response) {
+                        debbuger;
+                        for (var i = 0; i < response.length; i++) {
+                            el = response[i];
+                            el.Loc_Id = el["Loc-id"];
+                            delete el["Loc-id"];
+                        }
+                        return response;
+                    }
+                },
+                data: [],
+                error: function (e) {
+                    console.log('Error: ', e);
+                }
+            });
+        },
         createJSDODataSource: function () {
             try {
                 // create JSDO
@@ -74,31 +134,44 @@
                             }]
                         }
                     });
-                    this.jsdoDataSource = new kendo.data.DataSource({
-                        data: [],
-                        serverFiltering: true,
+                    this.jsdoDataSource = {
+                        prodrecno: null,
                         schema: {
                             model: eProduct
                         },
-                        error: function (e) {
-                            console.log("Error: ", e);
-                        },
-                    });
-                    //adding custom methods
-                    var me = this;
-                    this.jsdoDataSource.read = function () {
-                        debugger;
-                        var promisse
-                        var filter = me.jsdoDataSource.filter();
-                        promisse = me.jsdoModel.invoke('ProductDetail', { ProdRecno: filter.filters[0].value, CustId: "masroo" });
-                        promisse.done(function (session, result, details) {
-                            var currentProdList = details.response.dsProd.dsProd.eProduct;
-                            me.jsdoDataSource.data(currentProdList);
-                            if (currentProdList && currentProdList.length > 0) {
-                                kendo.bind($('#prodDetail'), currentProdList[0], kendo.mobile.ui);
+                        custid: "masroo",
+                        filter: function () {
+                            var f = {};
+                            if (this.prodrecno) {
+                                f.ProdRecno = this.prodrecno;
                             }
-                        });
-                    }
+                            if (this.custid) {
+                                f.CustId = this.custid;
+                            }
+                            return f;
+                        },
+                        transport: {
+                            // when the grid tries to read data, it will call this function
+                            read: function (options) {
+                                var me = app.viewModels.prodDetViewModel;
+                                var location = window.location.toString();
+                                me.jsdoDataSource.prodrecno = location.substring(location.lastIndexOf('?') + 4);
+                                var promise = me.jsdoModel.invoke('ProductDetail', me.jsdoDataSource.filter());
+                                promise.done(function (session, result, details) {
+                                    var currentProdList = details.response.dsProd.dsProd.eProduct;
+                                    options.success(currentProdList);
+                                    //assigning location list data
+                                    $("#prodDetailLocView").data("kendoMobileListView").dataSource.data(details.response.dsProd.dsProd.eProductLoc)
+                                });
+                                promise.fail(function () {
+                                    options.success([]);
+                                });
+                            }
+                        },
+                        error: function (e) {
+                            console.log('Error: ', e);
+                        }
+                    };
                 }
                 else {
                     console.log("Warning: jsdoSettings.resourceName not specified");
@@ -110,7 +183,7 @@
                 createDataSourceErrorFn({ errorObject: ex });
             }
         },
-        onHide() {
+        xxonHide() {
             app.clearData(prodDetViewModel);
         },
     });
