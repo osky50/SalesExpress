b383545d99a52f808ee12ea553e158532d8ceb10
Oscar Perez <oscar.perez@aperosolutions.com>
2016-10-26 16:06:32 -0600

    creating add review modal view

--- SalesExpress/viewModels/prodDetViewModel.js
+++ SalesExpress/viewModels/prodDetViewModel.js
@@ -42,7 +42,7 @@
                             if (button.name == 'reviews-link') {
                                 app.mobileApp.navigate('views/prodDetReviewsView.html');
                             }
-                        } catch (e) {}
+                        } catch (e) { }
                     },
                     dataBound: function (e) {
                         $('.rateit').each(function (index, element) {
@@ -185,6 +185,22 @@
             catch (ex) {
                 createDataSourceErrorFn({ errorObject: ex });
             }
+        },
+        addReview: function (e) {
+            debugger;
+            try {
+                var form = e.sender.element[0].parentNode;
+                var validator = $(form).kendoValidator({
+                    validateOnBlur: false
+                }).data('kendoValidator');
+                if (!validator.validateInput($(input)) || !validator.validateInput($(textarea)))
+                    return;
+            } catch (e) {
+
+            }
+        },
+        closeReview: function () {
+            $('#create_review').getKendoMobileModalView().close()
         },
     });
 
